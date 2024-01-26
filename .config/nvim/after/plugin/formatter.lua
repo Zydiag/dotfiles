53ccfcdc408c2ac5ca6b32715468a6f131024eed
Sahil <dshiv867@gmail.com>
2024-01-26 14:35:19 +0530

    added nvim

---
+++ .config/nvim/after/plugin/formatter.lua
@@ -0,0 +1,29 @@
+conform = require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		-- Conform will run multiple formatters sequentially
+		python = { "isort", "black" },
+		-- Use a sub-list to run only the first available formatter
+		javascript = { { "prettierd", "prettier" } },
+		typescript = { { "prettierd", "prettier" } },
+		html = { { "prettierd", "prettier" } },
+		css = { { "prettierd", "prettier" } },
+		json = { { "prettierd", "prettier" } },
+		javascriptreact = { { "prettierd", "prettier" } },
+		typescriptreact = { { "prettierd", "prettier" } },
+		markdown = { { "prettierd", "prettier" } },
+	},
+	format_on_save = {
+		lsp_fallback = true,
+		async = false,
+		timeout_ms = 500,
+	},
+})
+
+vim.keymap.set({ "n", "v" }, "<leader>fmf", function()
+	conform.format({
+		lsp_fallback = true,
+		async = false,
+		timeout_ms = 500,
+	})
+end)
